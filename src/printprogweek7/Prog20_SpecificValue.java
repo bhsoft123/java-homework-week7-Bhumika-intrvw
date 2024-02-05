372e268cce5a7f46b0dbd7ace3a5b6147a64fe2b
bhsoft123 <bhumika.softwaretesting@gmail.com>
2024-02-05 13:35:27 +0000

    java wk 7

---
+++ src/printprogweek7/Prog20_SpecificValue.java
@@ -0,0 +1,11 @@
+package printprogweek7;
+/*20. Write a Java program to test if an array contains a specific value.*/
+
+public class Prog20_SpecificValue {
+    //main method
+    public static void main(String[] args) {
+        String vowels [] = {"A","E","I","O","U"};
+        for (String s: vowels){
+            if ("E".equals(s)){
+                System.out.println("E found in the vowels list");}
+            } } }
